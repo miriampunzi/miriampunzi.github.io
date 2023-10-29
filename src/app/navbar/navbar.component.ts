ab2ad297359e1400639c9cfe78e574aac6479116
Miriam <mpunzi97@gmail.com>
2023-10-29 14:12:02 +0100

    Started adding css for about page, navbar and footer

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
 
+export class NavbarComponent implements OnInit {
+  isNavbarVisible: boolean = false;
+
+  ngOnInit() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 0) {
+        if (!this.isNavbarVisible) {
+          navbar?.classList.remove('navbar-invisible');
+          navbar?.classList.add('navbar-visible');
+          this.isNavbarVisible = true;
+        }
+      } else {
+        if (this.isNavbarVisible) {
+          navbar?.classList.remove('navbar-visible');
+          navbar?.classList.add('navbar-invisible');
+          this.isNavbarVisible = false;
+        }
+      }
+    });
+  }
 }
--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -9,6 +9,7 @@ export class ExperienceService {
     {
       company: "TXT e-Tech",
       jobTitle: "Software Engineer",
+      url: "https://www.txtgroup.com/",
       period: "Oct 2022 - current",
       location: "Milan, Italy",
       description: "I am creating Augmented and Virtual Reality training procedures and simulations for the Aerospace & Defense division.",
@@ -17,6 +18,7 @@ export class ExperienceService {
     {
       company: "EventLab",
       jobTitle: "Research Assistant in VR",
+      url: "https://www.event-lab.org/",
       period: "Apr 2022 - Sept 2022",
       location: "Barcelona, Spain",
       description: "I created by C# randomized concert scenarios with Unity & Visual Studio, and trained a neural network for Semantic Segmentation through Deep Learning",
@@ -25,6 +27,7 @@ export class ExperienceService {
     {
       company: "LISN",
       jobTitle: "UX Researcher & VR Programmer",
+      url: "https://www.lisn.upsaclay.fr/",
       period: "Apr 2021 - Sept 2021",
       location: "Paris, France",
       description: "I did the internship for my master thesis \"Nonverbal Behavior Reinforcement in Virtual Reality\"",
@@ -33,6 +36,7 @@ export class ExperienceService {
     {
       company: "MadHCILab",
       jobTitle: "VR Programmer",
+      url: "https://madhcilab.es/",
       period: "Feb 2020 - Mar 2020",
       location: "Madrid, Spain",
       description: "I participated in the development of a project for a driving school that simulates car accidents in VR in the context of distracted driving using Unity & Visual Studio",
@@ -41,6 +45,7 @@ export class ExperienceService {
     {
       company: "ENI Versalis",
       jobTitle: "Web Developer",
+      url: "https://versalis.eni.com/it-IT/home.html",
       period: "Jun 2015",
       location: "Mantova, Italy",
       description: "I programmed a web tool in HTML and Javascript for value conversion and pipe sizing for an industrial chemical plant",
