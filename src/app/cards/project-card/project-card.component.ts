dd89a59ad4e28b79fc172eabe1643407ae4f1dc3
Miriam <mpunzi97@gmail.com>
2024-11-12 19:12:54 +0100

    Updated home descriptions, rewritten project descriptions

--- src/app/cards/project-card/project-card.component.ts
+++ src/app/cards/project-card/project-card.component.ts
@@ -11,7 +11,7 @@ export class ProjectCardComponent {
   projects = [
     {
       title: "Racoomba!",
-      description: "Unity Game about 3 racoons who have to scrape items from a house, taking care to avoid the Roomba killers",
+      description: "Development of a Unity game about 3 racoons who have to scrape items from a house, taking care to avoid the Roomba killers",
       cover: "../../assets/images/project-covers/Cover Racoomba.png",
       link: "racoomba",
       keywords: ["Unity", "C#", "3D Modelling", "Game Design"],
@@ -19,7 +19,7 @@ export class ProjectCardComponent {
 
     {
       title: "Concert Randomization",
-      description: "Unity application that generates images to train a neural network for semantic segmentation applied to concert photos",
+      description: "Development of a Unity application that generates images to train a neural network for semantic segmentation applied to concert photos",
       cover: "../../assets/images/project-covers/Cover ConcertRandomization.jpg",
       link: "concert-randomization",
       keywords: ["Unity", "C#", "Machine Learning", "Semantic Segmentation"],
@@ -27,7 +27,7 @@ export class ProjectCardComponent {
 
     {
       title: "Nonverbal Behavior Reinforcement in VR",
-      description: "Virtual Reality game to teach nonverbal behavior to Agents through Reinforcement Learning.",
+      description: "Master Thesis: UX study and demo development of a VR game to teach nonverbal behavior to virtual agents through Reinforcement Learning",
       cover: "../../assets/images/project-covers/Cover NonverbalBehaviour.jpg",
       link: "nonverbal-behaviour",
       keywords: ["Unity", "C#", "VR", "Reinforcement Learning", "Gamification"],
@@ -35,7 +35,7 @@ export class ProjectCardComponent {
 
     {
       title: "Situated Memories",
-      description: "Augmented Reality mobile app that allows people to attach and retrieve digital media onto real objects.",
+      description: "UX study and demo development of an AR app to attach and retrieve digital media onto real objects",
       cover: "../../assets/images/project-covers/Cover SituatedMemories.jpg",
       link: "situated-memories",
       keywords: ["Unity", "C#", "AR", "Figma", "Mobile App"],
@@ -43,7 +43,7 @@ export class ProjectCardComponent {
 
     {
       title: "PopupBreak",
-      description: "Desktop app with Virtual Coach to support sedentary workers.",
+      description: "UX study and demo development of a desktop app with Virtual Coach to support sedentary workers",
       cover: "../../assets/images/project-covers/Cover PopupBreak.jpg",
       link: "popup-break",
       keywords: ["Figma", "Java Swing", "Avatar Design"],
@@ -51,7 +51,7 @@ export class ProjectCardComponent {
 
     {
       title: "InSignum",
-      description: "Interactive system to assist deaf people at home.",
+      description: "UX study and demo development of an interactive system to assist deaf people at home",
       cover: "../../assets/images/project-covers/Cover InSignum.jpg",
       link: "insignum",
       keywords: ["Android Studio", "Arduino", "User-Centered Design"],
@@ -59,7 +59,7 @@ export class ProjectCardComponent {
 
     {
       title: "Espoora - Make your stay more eventful",
-      description: "Design and Business analysis of Espoora, a platform for publishing, managing, and joining events in Espoo.",
+      description: "Design and Business analysis of Espoora, a platform for publishing, managing, and joining events in Espoo",
       cover: "../../assets/images/project-covers/Cover Espoora.png",
       link: "espoora",
       keywords: ["Design", "Business Modelling"],
@@ -67,7 +67,7 @@ export class ProjectCardComponent {
 
     {
       title: "Pearly",
-      description: "Mobile app where people can play tabletop games and game creators can test their new games.",
+      description: "UX study of a mobile app where people can play tabletop games and game creators can test their new games",
       cover: "../../assets/images/project-covers/Cover Pearly.jpg",
       link: "pearly",
       keywords: ["Figma", "Android Studio", "User-Centered Design"],
@@ -75,7 +75,7 @@ export class ProjectCardComponent {
 
     {
       title: "Bookflix",
-      description: "Application to find and reading books.",
+      description: "UX study of an application to find and to read books",
       cover: "../../assets/images/project-covers/Cover Bookflix.jpg",
       link: "bookflix",
       keywords: ["Figma", "User-Centered Design", "Usability Testing"],
